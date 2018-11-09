ff92e1e7a8229fbf1b56901255a20ecea1f5b024
Taweerat Chaiman <taweesoft@gmail.com>
2018-11-09 12:43:34 +0700

    change url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { app, proxy } = nodeProxy()
 
 const PORT = process.env.PORT || 5000
 
-app.use('/proxy', proxy)
+app.use('/', proxy)
 
 app.listen(PORT, () => {
   console.log(`API is running on port ${PORT} to ${config.get('api.endpoint')}`)
